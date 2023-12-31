2dafb1f228a302cf11be8d186311b2be563f6a5c
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2023-12-31 14:14:04 +0200

    Deleted react-query

--- my-app/app/components/Main.tsx
+++ my-app/app/components/Main.tsx
@@ -2,27 +2,23 @@
 'use client'
 import { useState, useEffect } from "react"
 import { db } from '../../../my-app/firebase'
-import { useQuery } from 'react-query'
 import 'firebase/firestore'
 import { Cards } from "./Cards";
-import { ArticlesService } from '../../../my-app/services/DatabaseServices'
 
 export const Main = () => {
 // Проверка на статус и входящие данные
-    const { data, status } = useQuery("articles", ArticlesService.getAll);
 // Получение данных 
     const [articles, setArticles] = useState([]);
+    console.log(articles)
 // Отправка запроса 
     useEffect(() => {
-        if (status === 'success') {
             const unsubscribe = db
                 .collection("articles")
                 .onSnapshot(snapshot => {
                     setArticles(snapshot.docs.map(doc => ({ docId: doc.id, ...doc.data() })))
                 });
             return () => unsubscribe()
-        }
-    }, [data])
+    }, [])
 // Проверка на ошибки и последующий рендеринг
     return (
         <div className="container container-content">
