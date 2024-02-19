0e449cb1a4af6abb13ba5549b318938070473ca8
Yaroslav Gorlenko <98014616+WarriorOFlonelinesS@users.noreply.github.com>
2024-02-19 18:49:46 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,9 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: "financial-blog-61c66.firebaseapp.com",
-  projectId: "financial-blog-61c66",
-  storageBucket: "financial-blog-61c66.appspot.com",
-  messagingSenderId: "844177809784",
-  appId: "1:844177809784:web:d3e98acc7152e19ce5288d",
+######################################
+  YOUR CONFIGURATION
+#####################################
 }
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
