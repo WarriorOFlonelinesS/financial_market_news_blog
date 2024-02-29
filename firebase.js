6e223a477a9555355840b0be16ea234794adb2dc
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2024-02-29 12:54:52 +0200

    fix some bugs

--- firebase.js
+++ firebase.js
@@ -4,12 +4,15 @@ import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-######################################
-  YOUR CONFIGURATION
-#####################################
-}
+  apiKey: "AIzaSyDKUCLhSJbUvZHpw_QnHDLo4jKf0bSTQJI",
+  authDomain: "financial-blog-61c66.firebaseapp.com",
+  projectId: "financial-blog-61c66",
+  storageBucket: "financial-blog-61c66.appspot.com",
+  messagingSenderId: "844177809784",
+  appId: "1:844177809784:web:d3e98acc7152e19ce5288d"
+};
+
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const db = app.firestore();
